6b9997af6bc0faefbc6addb512ade0b097355cae
Taras <221v92@gmail.com>
2020-08-31 19:57:39 +0300

    fix typo

--- purejs-examples/last_frame_time.js
+++ purejs-examples/last_frame_time.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const FRAMES_PER_SECOND = 30; // Valid values are 60,30,20,15,10...
-// set the mim time to render the next frame
+// set the min time to render the next frame
 const FRAME_MIN_TIME = (1000/60) * (60 / FRAMES_PER_SECOND) - (1000/60) * 0.5;
 var lastFrameTime = 0; // the last frame time
 
@@ -13,7 +13,7 @@ function update(time){
   }
   lastFrameTime = time; // remember the time of the rendered frame
   // render the frame
-  requestAnimationFrame(update); // get next farme
+  requestAnimationFrame(update); // get next frame
 }
 
 requestAnimationFrame(update); // start animation
